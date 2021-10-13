4be5cc8b361d306e2488a8cd12a1c03f4065b238
matjadach <mateusz.jadach@kpmg.co.uk>
2021-10-13 12:40:07 +0100

    Should commit more often

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 from todo_app.flask_config import Config
-from todo_app.data.session_items import _DEFAULT_TASKS, get_tasks
+from todo_app.data import session_items
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,29 +8,71 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html', _DEFAULT_TASKS=_DEFAULT_TASKS)
+    tasks = session_items.get_tasks()
+    return render_template('index.html', tasks = tasks)
 
+@app.route("/add", methods=['POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['Add']
+        session_items.add_task(title)
+    return index()
 
-# @app.route("/add", methods=["POST"])
-# def add():
-#     title = request.form.get("title")
-#     new_todo = Todo(title=title, complete=False)
-#     db.session.add(new_todo)
-#     db.session.commit()
-#     return redirect(url_for("home"))
+@app.route("/done/<id>")
+def done(id):
+    task = session_items.get_task(id)
+    task['status'] = 'Completed'
+    session_items.update_task(task)
+    return index()
 
+@app.route("/in_progress/<id>")
+def in_progress(id):
+    task = session_items.get_task(id)
+    task['status'] = 'In progress'
+    session_items.update_task(task)
+    return index()
 
-# @app.route("/update/<int:todo_id>")
-# def update(todo_id):
-#     todo = Todo.query.filter_by(id=todo_id).first()
-#     todo.complete = not todo.complete
-#     db.session.commit()
-#     return redirect(url_for("home"))
+@app.route("/not_started/<id>")
+def not_started(id):
+    task = session_items.get_task(id)
+    task['status'] = 'Not started'
+    session_items.update_task(task)
+    return index()
 
+@app.route("/delete/<id>")
+def delete(id):
+    id = id
+    task = session_items.get_task(id)
+    session_items.delete_task(id)
+    session_items.update_task(task)
+    return index()
 
-# @app.route("/delete/<int:todo_id>")
-# def delete(todo_id):
-#     todo = Todo.query.filter_by(id=todo_id).first()
-#     db.session.delete(todo)
-#     db.session.commit()
-#     return redirect(url_for("home"))
+@app.route("/update/<id>")
+def show_update_page(id):
+    task = session_items.get_task(id)
+    return render_template('update.html', id = id, task = task)
+    
+@app.route("/update/<id>/submit", methods = ['POST'])
+def update_task(id):
+    id = id
+    task = session_items.get_task(id)
+    new_title = request.form['Update']
+    task['title'] = new_title
+    session_items.update_task(task)
+    return index()    
+    
+@app.route("/sort_status")
+def sort_status():
+    session_items.sort_by_status()
+    return index()
+
+@app.route("/sort_desc")
+def sort_desc():
+    session_items.sort_by_title()
+    return index()
+
+@app.route("/sort_id")
+def sort_id():
+    session_items.sort_by_id()
+    return index()
+    
