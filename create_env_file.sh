a8ba7cdc60012450f03305110a3779eb90b80b97
Mateusz <matjadach@gmail.com>
2022-05-19 09:48:46 +0100

    renaming .env file

--- create_env_file.sh
+++ create_env_file.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd /home/runner/work/to-do-app/to-do-app/
-touch .env.testing
-echo "FLASK_APP=todo_app/app" >> .env.testing
-echo "FLASK_ENV=production" >> .env.testing
-echo "SECRET_KEY=secret-key" >> .env.testing
-echo $API_KEY >> .env.testing
-echo $API_TOKEN >> .env.testing
-echo $BOARD >> .env.testing
-echo $NOTSTARTED_LIST >> .env.testing
-echo $INPROGRESS_LIST >> .env.testing
-echo $COMPLETED_LIST >> .env.testing
+touch .env
+echo "FLASK_APP=todo_app/app" >> .env
+echo "FLASK_ENV=production" >> .env
+echo "SECRET_KEY=secret-key" >> .env
+echo $API_KEY >> .env
+echo $API_TOKEN >> .env
+echo $BOARD >> .env
+echo $NOTSTARTED_LIST >> .env
+echo $INPROGRESS_LIST >> .env
+echo $COMPLETED_LIST >> .env
