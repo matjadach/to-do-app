b901a5e21a9826e9bb75d29794d1c1a1bba2075e
Mateusz <matjadach@gmail.com>
2022-05-19 09:44:42 +0100

    adding sh script which creates .env file on the runner

---
+++ create_env_file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+touch .env.testing
+echo "FLASK_APP=todo_app/app" >> .env.testing
+echo "FLASK_ENV=production" >> .env.testing
+echo SECRET_KEY=secret-key >> .env.testing
+echo API_KEY >> .env.testing
+echo API_TOKEN >> .env.testing
+echo BOARD >> .env.testing
+echo NOTSTARTED_LIST >> .env.testing
+echo INPROGRESS_LIST >> .env.testing
+echo COMPLETED_LIST >> .env.testing
