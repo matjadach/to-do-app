e79f0420a85965fb7267a9919476db231cbbfc4a
Mateusz <matjadach@gmail.com>
2022-06-12 21:18:03 +0100

    finally?

--- create_env_file.sh
+++ create_env_file.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /home/runner/work/to-do-app/to-do-app/
-touch .env
-echo "FLASK_APP=todo_app/app" >> .env
-echo "FLASK_ENV=production" >> .env
-echo "SECRET_KEY=secret-key" >> .env
-echo $API_KEY >> .env
-echo $API_TOKEN >> .env
-echo $BOARD >> .env
-echo $NOTSTARTED_LIST >> .env
-echo $INPROGRESS_LIST >> .env
-echo $COMPLETED_LIST >> .env
+# cd /home/runner/work/to-do-app/to-do-app/
+touch .envtest
+echo "FLASK_APP=todo_app/app" >> .envtest
+echo "FLASK_envtest=production" >> .envtest
+echo "SECRET_KEY=secret-key" >> .envtest
+echo "API_KEY=$API_KEY" >> .envtest
+echo "API_TOKEN=$API_TOKEN" >> .envtest
+echo "BOARD=$BOARD" >> .envtest
+echo "NOTSTARTED_LIST=$NOTSTARTED_LIST" >> .envtest
+echo "INPROGRESS_LIST=$INPROGRESS_LIST" >> .envtest
+echo "COMPLETED_LIST=$COMPLETED_LIST" >> .envtest
