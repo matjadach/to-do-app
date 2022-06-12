6d4cc9a76434c167c0aefe3797b77ad4b5c3e97e
Mateusz <matjadach@gmail.com>
2022-06-12 22:09:07 +0100

    f

--- create_env_file.sh
+++ create_env_file.sh
@@ -4,9 +4,10 @@ touch .env
 echo "FLASK_APP=todo_app/app" >> .env
 echo "FLASK_.env=production" >> .env
 echo "SECRET_KEY=secret-key" >> .env
-echo "API_KEY=$API_KEY" >> .env
-echo "API_TOKEN=$API_TOKEN" >> .env
-echo "BOARD=$BOARD" >> .env
-echo "NOTSTARTED_LIST=$NOTSTARTED_LIST" >> .env
-echo "INPROGRESS_LIST=$INPROGRESS_LIST" >> .env
-echo "COMPLETED_LIST=$COMPLETED_LIST" >> .env
+echo "API_KEY=${{ secrets.API_KEY}}" >> .env
+echo "API_TOKEN=${{ secrets.API_TOKEN}}" >> .env
+echo "BOARD=${{ secrets.BOARD}}" >> .env
+echo "NOTSTARTED_LIST=${{ secrets.NOTSTARTED_LIST}}" >> .env
+echo "INPROGRESS_LIST=${{ secrets.INPROGRESS_LIST}}" >> .env
+echo "COMPLETED_LIST=${{ secrets.COMPLETED_LIST}}" >> .env
+
--- heroku_config.sh
+++ heroku_config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd /home/runner/work/to-do-app/to-do-app/
-heroku config:set `cat .env | grep FLASK_APP` -a app-do-to
-heroku config:set `cat .env | grep FLASK_ENV` -a app-do-to
-heroku config:set `cat .env | grep SECRET_KEY` -a app-do-to
-heroku config:set `cat .env | grep API_KEY` -a app-do-to
-heroku config:set `cat .env | grep API_TOKEN` -a app-do-to
-heroku config:set `cat .env | grep BOARD` -a app-do-to
-heroku config:set `cat .env | grep NOTSTARTED_LIST` -a app-do-to
-heroku config:set `cat .env | grep INPROGRESS_LIST` -a app-do-to
-heroku config:set `cat .env | grep COMPLETED_LIST` -a app-do-to
+heroku config:set FLASK_APP=todo_app/app -a app-do-to
+heroku config:set FLASK_ENV=production -a app-do-to
+heroku config:set SECRET_KEY=secret-key -a app-do-to
+heroku config:set API_KEY=${{ secrets.API_KEY}} -a app-do-to
+heroku config:set API_TOKEN=${{ secrets.API_TOKEN}} -a app-do-to
+heroku config:set BOARD=${{ secrets.BOARD}} -a app-do-to
+heroku config:set NOTSTARTED_LIST=${{ secrets.NOTSTARTED_LIST}} -a app-do-to
+heroku config:set INPROGRESS_LIST=${{ secrets.INPROGRESS_LIST}} -a app-do-to
+heroku config:set COMPLETED_LIST=${{ secrets.COMPLETED_LIST}} -a app-do-to
