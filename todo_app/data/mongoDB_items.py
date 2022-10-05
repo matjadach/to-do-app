9aff1c3090788aa82d96eee574f30a84c8ee3f49
Mateusz <matjadach@gmail.com>
2022-10-05 21:23:55 +0100

    troubleshooting syntax issues

--- todo_app/data/mongoDB_items.py
+++ todo_app/data/mongoDB_items.py
@@ -27,7 +27,7 @@ class MongoDBTasks:
 
         db_name = os.environ.get("TASKS_DB_NAME")
         collection_name = os.environ.get("TASKS_COLLECTION_NAME")
-        self.tasks = client[str(db_name)][str(collection_name)]
+        self.tasks = client[f"{db_name}"][f"{collection_name}"]
 
     def get_all_tasks(self):
         tasks_list = []
